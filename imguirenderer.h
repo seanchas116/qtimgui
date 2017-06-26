f6eef0ed68728dc1a05eda37840c42d6920a6660
Ryohei Ikegami <ikegami.ryohei@furyu.jp>
2017-06-26 14:07:02 +0900

    Move to private

--- imguirenderer.h
+++ imguirenderer.h
@@ -10,16 +10,6 @@ class ImGuiRenderer : QOpenGLExtraFunctions {
 public:
     ImGuiRenderer(ImGuiWidget *w) : widget(w) {}
 
-    ImGuiWidget *widget;
-    double       g_Time = 0.0f;
-    bool         g_MousePressed[3] = { false, false, false };
-    float        g_MouseWheel = 0.0f;
-    GLuint       g_FontTexture = 0;
-    int          g_ShaderHandle = 0, g_VertHandle = 0, g_FragHandle = 0;
-    int          g_AttribLocationTex = 0, g_AttribLocationProjMtx = 0;
-    int          g_AttribLocationPosition = 0, g_AttribLocationUV = 0, g_AttribLocationColor = 0;
-    unsigned int g_VboHandle = 0, g_VaoHandle = 0, g_ElementsHandle = 0;
-
     void initialize();
 
     void renderDrawList(ImDrawData *draw_data);
@@ -29,6 +19,17 @@ public:
 
     void onMousePressedChange(QMouseEvent *event);
     void onWheel(QWheelEvent *event);
+
+private:
+    ImGuiWidget *widget;
+    double       g_Time = 0.0f;
+    bool         g_MousePressed[3] = { false, false, false };
+    float        g_MouseWheel = 0.0f;
+    GLuint       g_FontTexture = 0;
+    int          g_ShaderHandle = 0, g_VertHandle = 0, g_FragHandle = 0;
+    int          g_AttribLocationTex = 0, g_AttribLocationProjMtx = 0;
+    int          g_AttribLocationPosition = 0, g_AttribLocationUV = 0, g_AttribLocationColor = 0;
+    unsigned int g_VboHandle = 0, g_VaoHandle = 0, g_ElementsHandle = 0;
 };
 
 }
